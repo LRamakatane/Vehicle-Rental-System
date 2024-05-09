b751e86f48fe862310f27de1e05beb77162c90e6
Hloni Ramakatane <hloniramakatane@Hlonis-MacBook-Air.local>
2024-05-09 22:17:40 +0200

    the beginning of the vehicle rental app

---
+++ src/main/java/com/example/demoWebClient/DemoWebClientApplication.java
@@ -0,0 +1,33 @@
+package com.example.demoWebClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.text.MessageFormat;
+
+@SpringBootApplication
+public class DemoWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebClientApplication.class, args);
+
+		String make = "volkswagen";
+
+		String url = MessageFormat.format("https://vpic.nhtsa.dot.gov/api//vehicles/GetModelsForMake/{0}?format=json", make);
+
+		WebClient.Builder builder = WebClient.builder();
+
+		String vehicle = builder.build()
+				.get()
+				.uri(url)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+		System.out.println("--------------------------------------------------------");
+		System.out.println(vehicle);
+		System.out.println("--------------------------------------------------------");
+	}
+
+}
